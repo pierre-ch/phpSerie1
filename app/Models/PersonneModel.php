997bae7c26d26bba290c49cca9684ac5377a650a
thomasdouillard <douillard.thomas@icloud.com>
2025-01-14 14:48:16 +0100

    commit

--- app/Models/PersonneModel.php
+++ app/Models/PersonneModel.php
@@ -2,7 +2,15 @@
 
 namespace Models;
 
+use Entity\Personne;
+
 class PersonneModel
 {
-
+    function getPersonnes(){
+        return [
+            new Personne('n1,p1,a1,s1'),
+            new Personne('n2,p2,a2,s2'),
+            new Personne('n3,p3,a3,s3'),
+        ];
+    }
 }
