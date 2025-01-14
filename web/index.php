5eb4eef44e1d6ae1e2f29c15f9d5352e0b2788fa
pierre-ch <96068634+pierre-ch@users.noreply.github.com>
2025-01-14 15:36:55 +0100

    index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
 <?php
-require_once "../vendor/autoload.php";
+
+$autoloadPath = "../vendor/autoload.php";
+
+if (!file_exists($autoloadPath)) {
+    die("Erreur : Le fichier d'autoload '$autoloadPath' est introuvable. Veuillez exécuter 'composer install' pour générer le fichier.");
+}
+
+// Charger l'autoloader
+require_once $autoloadPath;
+
 use Controllers\PersonneController;
 $pController = new PersonneController;
 $pController->list();
